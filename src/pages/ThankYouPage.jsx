9f538b893f287f0b36b64038dbaedcd95dc34342
Paulina <pg.gastol@gmail.com>
2026-04-29 17:24:55 +0200

    Add Thank You page: create a new component to display order confirmation message

---
+++ src/pages/ThankYouPage.jsx
@@ -0,0 +1,8 @@
+export default function ThankYouPage() {
+  return (
+    <main className="thank-you-page">
+      <h1>Thank You for Your Order!</h1>
+      <p>Your order has been placed successfully. We will process it soon.</p>
+    </main>
+  );
+}
