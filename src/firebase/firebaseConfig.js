5553ce26aebcf21423e1b80dd3b90e8eb3fbcf0b
Paulina <pg.gastol@gmail.com>
2026-05-07 14:41:18 +0200

    feat: add Firebase configuration and Firestore initialization
    - Added Firebase as a dependency in package.json.
    - Created firebaseConfig.js to initialize Firebase and Firestore with the project's configuration.

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFg5iSQLwxZJnnispb7yGFhvsDJeMEqmU",
+  authDomain: "beauty-shop-react.firebaseapp.com",
+  projectId: "beauty-shop-react",
+  storageBucket: "beauty-shop-react.firebasestorage.app",
+  messagingSenderId: "950059981991",
+  appId: "1:950059981991:web:125fee52e55831c6c38f82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
